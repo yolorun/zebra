e9af270b07ebe279ae9490bd0f752dc96a1a407e
v <chickenhun@gmail.com>
2025-12-04 22:19:25 +0100

    -

--- run_massive_slurm.sh
+++ run_massive_slurm.sh
@@ -83,10 +83,12 @@ echo "-------------------"
 # Run the training script
 echo "Starting training at $(date)..."
 
+# TODO try other connection strategies: connectivity_graph_pos.pkl, different min_connection_strength
+
 # Default command - modify parameters as needed via --params
 python massive_rnn_train.py --params sequence_length=32 batch_size=8 accumulate_grad_batches=4 max_epochs=10 \
        connectivity_path="connectivity_graph_global_threshold.pkl" min_connection_strength=0.52 \
-       hidden_dim=32 shared_stim_proj=False \
+       hidden_dim=32 shared_stim_proj=False noise_std=0.04 \
        strategy=auto precision="32" use_gradient_checkpointing=False
 
 if [ $? -eq 0 ]; then
